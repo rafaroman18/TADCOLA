4555b43c66df6ed24fd5f821f28320ea2df83978
rafaroman18 <33625416+rafaroman18@users.noreply.github.com>
2017-12-04 21:51:08 +0100

    Consigue hacerlo para valores que no se han repetido varias veces anteriormente (primera aparicion de a y b) y da fallo si alguno de los dos esta en el vector, arreglar eso.
    Ademas, no demasiado convencido de la estructura de la funcion, un poco sucia.

--- main.cpp
+++ main.cpp
@@ -2,54 +2,70 @@
 #include "tadpiladinamico.h"
 #include "tadcoladinamico.h"
 using namespace std;
-bool isomorfica (Pila<int> , Cola<int> );
-
+void invertir (Cola<int> *,int,int);
 int main()
 {
-    Pila<int> P;
-    Cola<int> C;
-    P.push(1);P.push(5);P.push(1);P.push(6);
-    C.push(13);C.push(6);C.push(13);C.push(5);
-    cout<<isomorfica(P,C);
+    int a=1,b=0;
+    Cola<int> C,H;
+    Cola<int> *c;
+    c=&C;
+    C.push(3);C.push(6);C.push(1);C.push(5);C.push(4);
+    C.push(6);C.push(8);C.push(2);C.push(3);C.push(0);
+    H=C;
+    while(!H.vacia())
+    {
+        cout<<H.frente()<<" ";
+        H.pop();
+    }
+    cout<<endl;
+    invertir(c,a,b);
+    while(!C.vacia())
+    {
+        cout<<C.frente()<<" ";
+        C.pop();
+    }
+
     return 0;
 }
 
-bool isomorfica (Pila<int> P, Cola<int> C)
+void invertir (Cola<int> *c,int a,int b)
 {
+    Cola<int> AUX;
+    Pila<int> P;
     bool valido=true;
-    Pila<int> AUXP=P;
-    Cola<int> AUXC=C;
-    int CONTP=0,CONTC=0;
-    while(!AUXP.vacia())
+    while(c->frente()!=a && valido)
     {
-        AUXP.pop();
-        CONTP++;
-    }
-    while(!AUXC.vacia())
-    {
-        AUXC.pop();
-        CONTC++;
-    }
-    int vecP[CONTP];
-    int vecC[CONTC];
-    int i;
-    for(i=0;i<CONTP;i++)
-    {
-        vecP[i]=P.tope();
-        P.pop();
-    }
-    for(i=0;i<CONTC;i++)
-    {
-        vecC[i]=C.frente();
-        C.pop();
-    }
-    if(CONTC>CONTP)
-    {
-        for(i=0;i<CONTP;i++)
+        AUX.push(c->frente());
+        c->pop();
+        if(c->vacia())
         {
-            
+            valido=false;
         }
     }
-
-    return valido;
+    while(c->frente()!=b && valido)
+    {
+        P.push(c->frente());
+        c->pop();
+        if(c->vacia())
+        {
+            valido= false;
+        }
+    }
+    P.push(b);
+    c->pop();
+    while(!P.vacia() && valido)
+    {
+        AUX.push(P.tope());
+        P.pop();
+    }
+    while(!c->vacia() && valido)
+    {
+        AUX.push(c->frente());
+        c->pop();
+    }
+    while(!AUX.vacia())
+    {
+        c->push(AUX.frente());
+        AUX.pop();
+    }
 }
